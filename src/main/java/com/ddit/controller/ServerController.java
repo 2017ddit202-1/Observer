ffdd33fd5af8ff914e507cad8617a2f9cc2652c5
Kim Kim <Kim@naver.com>
2017-02-11 11:31:44 +0900

    d

--- src/main/java/com/ddit/controller/ServerController.java
+++ src/main/java/com/ddit/controller/ServerController.java
@@ -1,20 +1,18 @@
 package com.ddit.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.http.HttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.ddit.service.AlertServiceImpl;
 
@@ -34,11 +32,18 @@ public class ServerController {
          HttpServletResponse response, Model model, HttpSession session) {
       String url = "server/serverMain";
 
-
+      
 		String loginUser = (String) session.getAttribute("loginUser");
 		String userOK = null;
 		String column="";
-
+		///////
+		request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+		String ip = request.getHeader("X-FORWARDED-FOR");
+		String host = request.getRemoteHost();
+		String serverName = request.getServerName();
+		if(ip == null){
+			ip = request.getRemoteAddr();
+		}
 		try {
 			userOK = alertService.select_sessionID(loginUser);  //alert테이블에 ID값이 존재하는지 select
 			column=alertService.authority_content(loginUser);
@@ -58,15 +63,16 @@ public class ServerController {
 				column ="USER로 등급신청 완료";
 			}
 			
-	
-			
-			
-			
 			System.out.println(column+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			model.addAttribute("userOK",userOK);
 			model.addAttribute("column", column);
 		}
-	
+		System.out.println(host + "()*()()()()()()(");
+		System.out.println(serverName + "()*()()()()()()(");
+		
+		model.addAttribute("clientIP", ip);
+		
+		
       return url;
 
    }
--- src/main/webapp/WEB-INF/views/server/serverMain.jsp
+++ src/main/webapp/WEB-INF/views/server/serverMain.jsp
@@ -61,8 +61,9 @@ $(document).ready(function() {
 
 
    <h1>채팅페이지</h1>
- <input type="text" id="message"/>
+ <input type="text" id="message" value="${clientIP}"/>
  <input type="button" id="sendMessage" value="메세지보내기"/>
+ 
  <div id="chatMessage" style="overflow: auto; max-height: 500px;"></div>
  
  <!-- //////////////////////////////////////////////////////////////// -->
