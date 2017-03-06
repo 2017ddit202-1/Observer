e7c0cf53b174d242950f5fd6d5d1397d5dfb1a67
Lim JG <stee90@naver.com>
2017-03-06 20:08:06 +0900

    알림내역 메일보내기

--- src/main/java/com/ddit/controller/NoticeController.java
+++ src/main/java/com/ddit/controller/NoticeController.java
@@ -1,12 +1,20 @@
 package com.ddit.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +28,22 @@ import com.ddit.service.MemberServiceImpl;
 import com.ddit.service.NoticeServiceImpl;
 import com.ddit.service.ServerServiceImpl;
 
+/**
+ * <pre>
+ * 
+ * </pre>
+ * @author 임종구
+ * @since 2017. 3. 6.
+ * @version 1.0
+ * @see javax.servlet.http.HttpServlet
+ * <pre>
+ * [[개정이력(Modification Information)]]
+ * 수정일                          수정자               수정내용
+ * --------     --------    ----------------------
+ * 2017. 3. 6.      pc20       최초작성
+ * Copyright (c) 2017 by DDIT All right reserved
+ * </pre>
+ */
 @Controller
 @RequestMapping("notice")
 public class NoticeController {
@@ -45,17 +69,12 @@ public class NoticeController {
 		this.memberGroupService = memberGroupService;
 	}
 	
-	
 	@RequestMapping("noticeList")
 	public String noticeList(HttpSession session,Model model,HttpServletRequest request){
 		
 		String url = "notice/notice";
-		String code = "";
 		String id = (String) session.getAttribute("loginUser");
 		String lice = null;
-		String memberGroup = null;
-		
-		String key = "";
 		String tpage = request.getParameter("tpage");
 
 		if (tpage == null) {
@@ -65,12 +84,8 @@ public class NoticeController {
 		}
 		
 		String paging = null;
-		
 		MemberVO memberVO = new MemberVO();
-		MemberGroupVO memberGroupVO = new MemberGroupVO();
-		ServerVO serverVO = new ServerVO();
 		List<NoticeVO> noticeList = null;
-		NoticeVO noticeVO = null;
 		try {
 			memberVO = memberService.selectMember(id);
 			lice = memberVO.getMem_group_lice();
@@ -79,17 +94,18 @@ public class NoticeController {
 				paging = noticeService.totalNoticelist(Integer.parseInt(tpage),lice);
 			}
 			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
 		
 		request.setAttribute("noticeList", noticeList);
 		int n = noticeList.size();
 		request.setAttribute("memberListSize", n);
 		request.setAttribute("paging", paging);
 		
-		return url;
 	}
+		return url;
 	
+}
 }
