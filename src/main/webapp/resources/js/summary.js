ba9f8124dcc4b0ab040bc9370d6dd47770955d6e
Lim JG <stee90@naver.com>
2017-03-06 17:06:16 +0900

    요약정보 수정

--- src/main/webapp/resources/js/summary.js
+++ src/main/webapp/resources/js/summary.js
@@ -1,39 +1,41 @@
 var inter="";
 var inter2="";
 $(function(){
- 	inter  = setInterval("tt()",5000);
-	 $("#loader").show();
-//	 $('#summaryTb').hide();
-//	tt();
-//	serverInfo();
-	
+   inter  = setInterval("tt()",5000);
+    $("#loader").show();
+    $("#btnZxc").hide();
+    $("#btntotal").hide();
+    $('#foot').hide();
+//    tt();
+//   serverInfo();
+   
 });
 
 function serverInfo(){
-		$.ajax({
-			url:"serverInfo",
-			type:'post',
-			dataType:'json',
-			success:function(data){
-				var tb = 	'<div id="summaryTb">'
-							+'<table>'
-							+'<tr style="border:1px solid red;">'
-							+'<td style="border:1px solid red;">운영체제</td>'
-							+'<td style="border:1px solid red;">서버명</td>'
-							+'<td style="border:1px solid red;">IP 주소 </td>'
-							+'<td style="border:1px solid red;">BIT</td>'
-							+'</tr>'
-							+'<tr style="border:1px solid red;">'
-							+'<td style="border:1px solid red;">'+data.server_host+'</td>'
-							+'<td style="border:1px solid red;">'+data.server_os_name+'</td>'
-							+'<td style="border:1px solid red;">'+data.server_ip+'</td>'
-							+'<td style="border:1px solid red;">'+data.server_os_support+'</td>'
-							+'</tr>'
-							+'</table>'
-							+'</div>'
-					$('#serverInfo').html(tb);
-			}
-	});
+      $.ajax({
+         url:"serverInfo",
+         type:'post',
+         dataType:'json',
+         success:function(data){
+            var tb =    '<div id="summaryTb">'
+                     +'<table>'
+                     +'<tr style="border:1px solid red;">'
+                     +'<td style="border:1px solid red;">운영체제</td>'
+                     +'<td style="border:1px solid red;">서버명</td>'
+                     +'<td style="border:1px solid red;">IP 주소 </td>'
+                     +'<td style="border:1px solid red;">BIT</td>'
+                     +'</tr>'
+                     +'<tr style="border:1px solid red;">'
+                     +'<td style="border:1px solid red;">'+data.server_host+'</td>'
+                     +'<td style="border:1px solid red;">'+data.server_os_name+'</td>'
+                     +'<td style="border:1px solid red;">'+data.server_ip+'</td>'
+                     +'<td style="border:1px solid red;">'+data.server_os_support+'</td>'
+                     +'</tr>'
+                     +'</table>'
+                     +'</div>'
+               $('#serverInfo').html(tb);
+         }
+   });
 };
 
 
@@ -78,107 +80,110 @@ function syncExtremes(e) {
 }
 
 function tt() {
-	$('#container').empty();
-	$.ajax({
-		url:"summaryPage",
-		type:'post',
-		dataType:'json',
-		success:function(data){
-			 $("#loader").hide();
-			 serverInfo();
-			 var time = "";
-				$.each(data.xData,function(i,date){
-					var dat = new Date(data.xData[i]);
-					var hours = dat.getHours();
-			        var minutes = dat.getMinutes();
-			        var FullDate = hours +":"+minutes;
-			        if(i == 0){
-				        time = [FullDate];
-			        }
-			        
-			        if(i > 0){
-			        	time.push(FullDate);
-			        }
-					
-				});
-			
-				$.each(data.datasets, function(i, dataset) {
-					$.each(dataset.data, function(j, ff) {
-						if (j == 0) {
-							total = [ window.parseInt(ff) ];
-						}
-						if (j > 0) {
-							total.push(parseInt(ff));
-						}
-					});
-					$('<div class="chart" id="page-wrapper6">').appendTo('#container').highcharts(
-							{
-								chart : {
-									marginLeft : 40, // Keep all charts left aligned
-									spacingTop : 20,
-									spacingBottom : 20,
-									height:300,
-						            width:600,
-						            marginTop:60,
-						            marginLeft: 50
-								},
-								title : {
-									text : dataset.name,
-									align : 'left',
-									margin : 0,
-									x : 30,
-									style:{
-										fontSize:'20px',
-										
-									}
-								},
-								credits : {
-									enabled : false
-								},
-								legend : {
-									enabled : false
-								},
-								xAxis : {
-									gridLineColor:'#ffffff',
-									crosshair : true,
-									categories:time,
-									tickInterval: 4,
-									labels: {
-					                      rotation: 0,
-					                      align: 'center'
-					            	  }
-								},
-							    plotOptions: {
-							        series: {
-							            marker: {
-							                enabled: false
-							            }
-							        }
-							    },
-								yAxis : {
-									  minorGridLineColor:'#ffffff',
-									title : {
-										text : null
-									},
-									  labels: {
-						                    formatter: function () {
-						                        return this.value + '%';
-						                    }
-						                }
-								},
+   $.ajax({
+      url:"summaryPage",
+      type:'post',
+      dataType:'json',
+      success:function(data){
+    	  $('#container').empty();
+          $("#loader").hide();
+          $("#btnZxc").show();
+          $("#btntotal").show();
+          $('#foot').show();
+          serverInfo();
+         var time = "";
+            $.each(data.xData,function(i,date){
+               var dat = new Date(data.xData[i]);
+               var hours = dat.getHours();
+                 var minutes = dat.getMinutes();
+                 var FullDate = hours +":"+minutes;
+                 if(i == 0){
+                    time = [FullDate];
+                 }
+                 
+                 if(i > 0){
+                    time.push(FullDate);
+                 }
+               
+            });
+         
+            $.each(data.datasets, function(i, dataset) {
+               $.each(dataset.data, function(j, ff) {
+                  if (j == 0) {
+                     total = [ window.parseInt(ff) ];
+                  }
+                  if (j > 0) {
+                     total.push(parseInt(ff));
+                  }
+               });
+               $('<div class="chart" id="page-wrapper6">').appendTo('#container').highcharts(
+                     {
+                        chart : {
+                           marginLeft : 40, // Keep all charts left aligned
+                           spacingTop : 20,
+                           spacingBottom : 20,
+                           height:300,
+                              width:600,
+                              marginTop:60,
+                              marginLeft: 50
+                        },
+                        title : {
+                           text : dataset.name,
+                           align : 'left',
+                           margin : 0,
+                           x : 30,
+                           style:{
+                              fontSize:'20px',
+                              
+                           }
+                        },
+                        credits : {
+                           enabled : false
+                        },
+                        legend : {
+                           enabled : false
+                        },
+                        xAxis : {
+                           gridLineColor:'#ffffff',
+                           crosshair : true,
+                           categories:time,
+                           tickInterval: 4,
+                           labels: {
+                                     rotation: 0,
+                                     align: 'center'
+                                }
+                        },
+                         plotOptions: {
+                             series: {
+                                 marker: {
+                                     enabled: false
+                                 }
+                             }
+                         },
+                        yAxis : {
+                             minorGridLineColor:'#ffffff',
+                           title : {
+                              text : null
+                           },
+                             labels: {
+                                      formatter: function () {
+                                          return this.value + '%';
+                                      }
+                                  }
+                        },
 
-								series : [ {
-									data : total,
-									name : dataset.name,
-									type : dataset.type,
-									color : Highcharts.getOptions().colors[i],
-									fillOpacity : 0.3,
-									tooltip : {
-										valueSuffix : ' ' + dataset.unit
-									}
-								} ]
-							});
-				});
-			}
-		});
-	};
+                        series : [ {
+                           data : total,
+                           name : dataset.name,
+                           type : dataset.type,
+                           color : Highcharts.getOptions().colors[i],
+                           fillOpacity : 0.3,
+                           tooltip : {
+                              valueSuffix : ' ' + dataset.unit
+                           }
+                        } ]
+                     });
+            });
+         }
+      });
+   };
